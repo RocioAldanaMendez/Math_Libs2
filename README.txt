06cb1b96391a07127b8bf14c9e72b848752a23b6
RocioAldanaMendez <rocioalmendez@gmail.com>
2022-10-05 13:04:57 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+A small game was created where the user enters 4 words and a 
+text is completed with them.
+
+Here it was practiced, requests to the user and concatenation.
+
+Enjoy!
---
+++ crazy_stories.py
@@ -0,0 +1,8 @@
+adj = input("Adjetive: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+sust_plu = input("Noun: ")
+
+madlib = f"¡Programing is so {adj}! It's always excites me because I love {verb1} problems. ¡Learn to {verb2}  with <<Rocio Mendez Companies>> and reach yours {sust_plu}!"
+
+print(madlib)
