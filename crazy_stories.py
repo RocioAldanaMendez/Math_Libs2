052692bafc587e64bed13f6ade62f9e60f5a459b
RocioAldanaMendez <rocioalmendez@gmail.com>
2022-11-19 17:07:13 -0300

    subiendo prueba

--- crazy_stories.py
+++ crazy_stories.py
@@ -6,3 +6,5 @@ sust_plu = input("Noun: ")
 madlib = f"¡Programing is so {adj}! It's always excites me because I love {verb1} problems. ¡Learn to {verb2}  with <<Rocio Mendez Companies>> and reach yours {sust_plu}!"
 
 print(madlib)
+
+print("inserting test code")
